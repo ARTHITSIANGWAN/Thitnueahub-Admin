d9ee1499a14e1fce253d185c33357c6edab94b5b
Thitnueahub Admin <241469782+ARTHITSIANGWAN@users.noreply.github.com>
2026-07-15 05:38:29 +0700

    Update fivetwo.ml

--- fivetwo.ml
+++ fivetwo.ml
@@ -1,1 +1,23 @@
+(* [project] tnh_ai_master_template *)
+(* [purpose] dna_governance: strict 5s edge constraints *)
 
+type rule_5s = {
+  seiri    : bool;
+  seiton   : bool;
+  seiso    : bool;
+  seiketsu : bool;
+  shitsuke : bool;
+}
+
+let active_governance = {
+  seiri    = true;
+  seiton   = true;
+  seiso    = true;
+  seiketsu = true;
+  shitsuke = true;
+}
+
+let verify_clean_code status =
+  if status = "clean" then "zero_garbage_approved"
+  else "unauthorized_garbage_detected"
+  
